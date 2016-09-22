e348e9577de35df11413517d24607685048f490e
felpietscardi <felpietscardi@gmail.com>
2016-09-22 18:26:47 +0200

    implemented test method to test the robot.place() method

--- src/test/java/au/com/rea/model/impl/RobotImplTest.java
+++ src/test/java/au/com/rea/model/impl/RobotImplTest.java
@@ -2,18 +2,33 @@ package au.com.rea.model.impl;
 
 import static org.junit.Assert.fail;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import au.com.rea.model.Position;
+import au.com.rea.model.Position.Orientation;
+import au.com.rea.model.Robot;
+
 public class RobotImplTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	private Robot robot = null;
+	
+	@Before
+	public void setup() {
+		robot = new RobotImpl(1, new Position());
 	}
-
+	
 	@Test
 	public void testPlace() {
-		fail("Not yet implemented");
+		assert robot.getPosition().getX() == 0;
+		assert robot.getPosition().getY() == 0;
+		assert robot.getPosition().getOrientation() == Orientation.NORTH;
+	
+		Position newPosition = new Position(5, 7, Orientation.SOUTH);
+		robot.place(newPosition);
+		
+		assert robot.getPosition().getX() == 5;
+		assert robot.getPosition().getY() == 7;
+		assert robot.getPosition().getOrientation() == Orientation.SOUTH;
 	}
 
 	@Test
