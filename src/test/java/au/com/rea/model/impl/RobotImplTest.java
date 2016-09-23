296745e47437936b0c0bc4b9a32feb5706133f4d
felpietscardi <felpietscardi@gmail.com>
2016-09-23 20:26:07 +0200

    updated the Robot test to test the boundaries and added exception handling

--- src/test/java/au/com/rea/model/impl/RobotImplTest.java
+++ src/test/java/au/com/rea/model/impl/RobotImplTest.java
@@ -5,162 +5,226 @@ import static org.junit.Assert.fail;
 import org.junit.Before;
 import org.junit.Test;
 
+import au.com.rea.exception.InvalidMovementException;
 import au.com.rea.model.Position;
 import au.com.rea.model.Position.Orientation;
 import au.com.rea.model.Robot;
+import au.com.rea.model.TerrainData;
 
 public class RobotImplTest {
 	private Robot robot = null;
-	
+
 	@Before
 	public void setup() {
-		robot = new RobotImpl(1, new Position());
+		robot = new RobotImpl(1, new TerrainData(5, 5));
 	}
-	
+
 	@Test
 	public void testPlace() {
-		assert robot.getPosition().getX() == 0;
-		assert robot.getPosition().getY() == 0;
-		assert robot.getPosition().getOrientation() == Orientation.NORTH;
-	
-		Position newPosition = new Position(5, 7, Orientation.SOUTH);
-		robot.place(newPosition);
-		
-		assert robot.getPosition().getX() == 5;
-		assert robot.getPosition().getY() == 7;
-		assert robot.getPosition().getOrientation() == Orientation.SOUTH;
+		// placing robot with in normal range
+		try {
+			assert robot.getPosition().getX() == 0;
+			assert robot.getPosition().getY() == 0;
+			assert robot.getPosition().getOrientation() == Orientation.NORTH;
+
+			Position newPosition = new Position(4, 2, Orientation.SOUTH);
+			robot.place(newPosition);
+
+			assert robot.getPosition().getX() == 4;
+			assert robot.getPosition().getY() == 2;
+			assert robot.getPosition().getOrientation() == Orientation.SOUTH;
+		} catch (InvalidMovementException e) {
+			fail("Unexpected error!");
+		}
+
+		// placing robot outside of range
+		try {
+			robot.place(new Position(8, -1, Orientation.EAST));
+			fail("Expected an InvalidMovementException to be thrown");
+		} catch (InvalidMovementException e) {
+			assert e.getMessage().equals("InvalidMovementException: Moving forward will result in death!");
+		}
 	}
 
 	@Test
 	public void testMoveForward() {
-		// test east
-		Position newPosition = new Position(3, 3, Orientation.EAST);
-		robot.place(newPosition);
-		robot.moveForward();
-		
-		assert robot.getPosition().getX() == 4;
-		assert robot.getPosition().getY() == 3;
-		assert robot.getPosition().getOrientation() == Orientation.EAST;
+		try {
+			// test east
+			Position newPosition = new Position(3, 3, Orientation.EAST);
+			robot.place(newPosition);
+			robot.moveForward();
 
-		// test north
-		newPosition = new Position(3, 3, Orientation.NORTH);
-		robot.place(newPosition);
-		robot.moveForward();
-		
-		assert robot.getPosition().getX() == 3;
-		assert robot.getPosition().getY() == 4;
-		assert robot.getPosition().getOrientation() == Orientation.NORTH;
+			assert robot.getPosition().getX() == 4;
+			assert robot.getPosition().getY() == 3;
+			assert robot.getPosition().getOrientation() == Orientation.EAST;
 
-		// test west
-		newPosition = new Position(3, 3, Orientation.WEST);
-		robot.place(newPosition);
-		robot.moveForward();
-		
-		assert robot.getPosition().getX() == 2;
-		assert robot.getPosition().getY() == 3;
-		assert robot.getPosition().getOrientation() == Orientation.WEST;
+			// test north
+			newPosition = new Position(3, 3, Orientation.NORTH);
+			robot.place(newPosition);
+			robot.moveForward();
 
-		// test south
-		newPosition = new Position(3, 3, Orientation.SOUTH);
-		robot.place(newPosition);
-		robot.moveForward();
-		
-		assert robot.getPosition().getX() == 3;
-		assert robot.getPosition().getY() == 2;
-		assert robot.getPosition().getOrientation() == Orientation.SOUTH;
+			assert robot.getPosition().getX() == 3;
+			assert robot.getPosition().getY() == 4;
+			assert robot.getPosition().getOrientation() == Orientation.NORTH;
+
+			// test west
+			newPosition = new Position(3, 3, Orientation.WEST);
+			robot.place(newPosition);
+			robot.moveForward();
+
+			assert robot.getPosition().getX() == 2;
+			assert robot.getPosition().getY() == 3;
+			assert robot.getPosition().getOrientation() == Orientation.WEST;
+
+			// test south
+			newPosition = new Position(3, 3, Orientation.SOUTH);
+			robot.place(newPosition);
+			robot.moveForward();
+
+			assert robot.getPosition().getX() == 3;
+			assert robot.getPosition().getY() == 2;
+			assert robot.getPosition().getOrientation() == Orientation.SOUTH;
+		} catch (InvalidMovementException e) {
+			fail("Unexpected error!");
+		}
+
+		// test exception - crossing the boundary
+		try {
+			// check bottom boundary
+			robot.place(new Position(0, 0, Orientation.SOUTH));
+			robot.moveForward();
+			fail("Expected an InvalidMovementException to be thrown");
+		} catch (InvalidMovementException e) {
+			assert e.getMessage().equals("InvalidMovementException: Moving forward will result in death!");
+		}
+
+		try {
+			// check left boundary
+			robot.place(new Position(0, 0, Orientation.WEST));
+			robot.moveForward();
+			fail("Expected an InvalidMovementException to be thrown");
+		} catch (InvalidMovementException e) {
+			assert e.getMessage().equals("InvalidMovementException: Moving forward will result in death!");
+		}
+
+		try {
+			// check top boundary
+			robot.place(new Position(4, 4, Orientation.NORTH));
+			robot.moveForward();
+			fail("Expected an InvalidMovementException to be thrown");
+		} catch (InvalidMovementException e) {
+			assert e.getMessage().equals("InvalidMovementException: Moving forward will result in death!");
+		}
+
+		try {
+			// check right boundary
+			robot.place(new Position(4, 4, Orientation.EAST));
+			robot.moveForward();
+			fail("Expected an InvalidMovementException to be thrown");
+		} catch (InvalidMovementException e) {
+			assert e.getMessage().equals("InvalidMovementException: Moving forward will result in death!");
+		}
 	}
 
 	@Test
 	public void testTurnRight() {
-		// test east
-		Position newPosition = new Position(3, 3, Orientation.EAST);
-		robot.place(newPosition);
-		robot.turnRight();
-		
-		assert robot.getPosition().getX() == 3;
-		assert robot.getPosition().getY() == 3;
-		assert robot.getPosition().getOrientation() == Orientation.SOUTH;
+		try {
+			// test east
+			Position newPosition = new Position(3, 3, Orientation.EAST);
+			robot.place(newPosition);
+			robot.turnRight();
 
-		// test north
-		newPosition = new Position(3, 3, Orientation.NORTH);
-		robot.place(newPosition);
-		robot.turnRight();
-		
-		assert robot.getPosition().getX() == 3;
-		assert robot.getPosition().getY() == 3;
-		assert robot.getPosition().getOrientation() == Orientation.EAST;
+			assert robot.getPosition().getX() == 3;
+			assert robot.getPosition().getY() == 3;
+			assert robot.getPosition().getOrientation() == Orientation.SOUTH;
 
-		// test west
-		newPosition = new Position(3, 3, Orientation.WEST);
-		robot.place(newPosition);
-		robot.turnRight();
-		
-		assert robot.getPosition().getX() == 3;
-		assert robot.getPosition().getY() == 3;
-		assert robot.getPosition().getOrientation() == Orientation.NORTH;
+			// test north
+			newPosition = new Position(3, 3, Orientation.NORTH);
+			robot.place(newPosition);
+			robot.turnRight();
 
-		// test south
-		newPosition = new Position(3, 3, Orientation.SOUTH);
-		robot.place(newPosition);
-		robot.turnRight();
-		
-		assert robot.getPosition().getX() == 3;
-		assert robot.getPosition().getY() == 3;
-		assert robot.getPosition().getOrientation() == Orientation.WEST;
+			assert robot.getPosition().getX() == 3;
+			assert robot.getPosition().getY() == 3;
+			assert robot.getPosition().getOrientation() == Orientation.EAST;
+
+			// test west
+			newPosition = new Position(3, 3, Orientation.WEST);
+			robot.place(newPosition);
+			robot.turnRight();
+
+			assert robot.getPosition().getX() == 3;
+			assert robot.getPosition().getY() == 3;
+			assert robot.getPosition().getOrientation() == Orientation.NORTH;
+
+			// test south
+			newPosition = new Position(3, 3, Orientation.SOUTH);
+			robot.place(newPosition);
+			robot.turnRight();
+
+			assert robot.getPosition().getX() == 3;
+			assert robot.getPosition().getY() == 3;
+			assert robot.getPosition().getOrientation() == Orientation.WEST;
+		} catch (InvalidMovementException e) {
+			fail("Unexpected error!");
+		}
 	}
 
 	@Test
 	public void testTurnLeft() {
-		// test east
-		Position newPosition = new Position(3, 3, Orientation.EAST);
-		robot.place(newPosition);
-		robot.turnLeft();
-		
-		assert robot.getPosition().getX() == 3;
-		assert robot.getPosition().getY() == 3;
-		assert robot.getPosition().getOrientation() == Orientation.NORTH;
+		try {
+			// test east
+			Position newPosition = new Position(3, 3, Orientation.EAST);
+			robot.place(newPosition);
+			robot.turnLeft();
 
-		// test north
-		newPosition = new Position(3, 3, Orientation.NORTH);
-		robot.place(newPosition);
-		robot.turnLeft();
-		
-		assert robot.getPosition().getX() == 3;
-		assert robot.getPosition().getY() == 3;
-		assert robot.getPosition().getOrientation() == Orientation.WEST;
+			assert robot.getPosition().getX() == 3;
+			assert robot.getPosition().getY() == 3;
+			assert robot.getPosition().getOrientation() == Orientation.NORTH;
 
-		// test west
-		newPosition = new Position(3, 3, Orientation.WEST);
-		robot.place(newPosition);
-		robot.turnLeft();
-		
-		assert robot.getPosition().getX() == 3;
-		assert robot.getPosition().getY() == 3;
-		assert robot.getPosition().getOrientation() == Orientation.SOUTH;
+			// test north
+			newPosition = new Position(3, 3, Orientation.NORTH);
+			robot.place(newPosition);
+			robot.turnLeft();
 
-		// test south
-		newPosition = new Position(3, 3, Orientation.SOUTH);
-		robot.place(newPosition);
-		robot.turnLeft();
-		
-		assert robot.getPosition().getX() == 3;
-		assert robot.getPosition().getY() == 3;
-		assert robot.getPosition().getOrientation() == Orientation.EAST;
+			assert robot.getPosition().getX() == 3;
+			assert robot.getPosition().getY() == 3;
+			assert robot.getPosition().getOrientation() == Orientation.WEST;
+
+			// test west
+			newPosition = new Position(3, 3, Orientation.WEST);
+			robot.place(newPosition);
+			robot.turnLeft();
+
+			assert robot.getPosition().getX() == 3;
+			assert robot.getPosition().getY() == 3;
+			assert robot.getPosition().getOrientation() == Orientation.SOUTH;
+
+			// test south
+			newPosition = new Position(3, 3, Orientation.SOUTH);
+			robot.place(newPosition);
+			robot.turnLeft();
+
+			assert robot.getPosition().getX() == 3;
+			assert robot.getPosition().getY() == 3;
+			assert robot.getPosition().getOrientation() == Orientation.EAST;
+		} catch (InvalidMovementException e) {
+			fail("Unexpected error!");
+		}
+
 	}
 
 	@Test
 	public void testReportStatus() {
-		assert robot.reportStatus().equals("WALL-E says: \"I am at (0, 0) facing NORTH.\"");
-		
-		Position newPosition = new Position(2, 1, Orientation.EAST);
-		robot.place(newPosition);
+		try {
+			assert robot.reportStatus().equals("WALL-E says: \"I am at (0, 0) facing NORTH.\"");
 
-		assert robot.reportStatus().equals("WALL-E says: \"I am at (2, 1) facing EAST.\"");
-	}
-	
-	@Test
-	public void testBoundary() {
-		fail("Not yet implemented");
-	}
+			Position newPosition = new Position(2, 1, Orientation.EAST);
+			robot.place(newPosition);
 
+			assert robot.reportStatus().equals("WALL-E says: \"I am at (2, 1) facing EAST.\"");
+		} catch (InvalidMovementException e) {
+			fail("Unexpected error!");
+		}
+
+	}
 }
