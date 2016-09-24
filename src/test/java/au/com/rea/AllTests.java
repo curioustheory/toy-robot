1a6ae11258d48e2d40cf8cd8aeb2b8b6dfcd44a4
felpietscardi <felpietscardi@gmail.com>
2016-09-24 21:45:04 +0200

    added RobotRuleEngineTest to the AllTest suite

--- src/test/java/au/com/rea/AllTests.java
+++ src/test/java/au/com/rea/AllTests.java
@@ -4,11 +4,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import au.com.rea.engine.RobotRuleEngineTest;
 import au.com.rea.model.impl.RobotImplTest;
 import au.com.rea.util.RobotCommandUtilTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ RobotImplTest.class, RobotCommandUtilTest.class })
+@SuiteClasses({ RobotImplTest.class, RobotCommandUtilTest.class, RobotRuleEngineTest.class })
 public class AllTests {
 
 }
