19efe1110f63809824239ea1d752849281880051
felpietscardi <felpietscardi@gmail.com>
2016-09-24 20:21:02 +0200

    added RobotCommandUtilTest to the test suite

--- src/test/java/au/com/rea/AllTests.java
+++ src/test/java/au/com/rea/AllTests.java
@@ -1,17 +1,14 @@
 package au.com.rea;
+
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import au.com.rea.engine.RobotRuleEngineTest;
 import au.com.rea.model.impl.RobotImplTest;
 import au.com.rea.util.RobotCommandUtilTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({
-	RobotImplTest.class, 
-	RobotRuleEngineTest.class,
-	RobotCommandUtilTest.class})
+@SuiteClasses({ RobotImplTest.class, RobotCommandUtilTest.class })
 public class AllTests {
 
 }
