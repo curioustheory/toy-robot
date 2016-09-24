61dfb696b92fbebbf854219f32cb10fdb97018b8
felpietscardi <felpietscardi@gmail.com>
2016-09-24 20:20:15 +0200

    implemented RobotCommandUtil command parser and the unit test cases

--- src/main/java/au/com/rea/util/RobotCommandUtil.java
+++ src/main/java/au/com/rea/util/RobotCommandUtil.java
@@ -1,13 +1,80 @@
 package au.com.rea.util;
 
+import java.util.Queue;
+import java.util.regex.Pattern;
+
+import au.com.rea.model.Actionable;
+import au.com.rea.model.Position;
+import au.com.rea.model.Position.Orientation;
+import au.com.rea.model.impl.action.LeftAction;
+import au.com.rea.model.impl.action.MoveAction;
+import au.com.rea.model.impl.action.PlaceAction;
+import au.com.rea.model.impl.action.ReportAction;
+import au.com.rea.model.impl.action.RightAction;
+
 /**
  * 
  * @author Tony Wang
  *
  */
 public class RobotCommandUtil {
-	
-	// TODO: create string parser with tokenizer & action accessor that takes in a sequence of action
-	// TODO: define the table
+	private static Pattern patternWord = Pattern.compile("[A-Z]*");
+	private static Pattern patternPosition = Pattern.compile("[0-9]*,[0-9]*,[A-Z]*");
 
+	public enum CommandAction {
+		PLACE, MOVE, LEFT, RIGHT, REPORT
+	}
+
+	public static void validCommandAndCreateAction(Queue<Actionable> queue, String input) throws IllegalArgumentException {
+		if (!input.isEmpty()) {
+			if (input.startsWith(CommandAction.PLACE.name())) {
+				String[] words = input.trim().split(" ", 3);
+				if (patternWord.matcher(words[0].trim()).matches() && patternPosition.matcher(words[1].trim()).matches()) {
+					queue.add(createAction(words[0], words[1]));
+					if (words.length > 2) {
+						validCommandAndCreateAction(queue, words[2].trim());
+					}
+				} else {
+					throw new IllegalArgumentException("Unknown command: " + words[0] + " " + words[1]);
+				}
+			} else {
+				String[] words = input.trim().split(" ", 2);
+				if (patternWord.matcher(words[0].trim()).matches()) {
+					queue.add(createAction(words[0], ""));
+					if (words.length > 1) {
+						validCommandAndCreateAction(queue, words[1].trim());
+					}
+				} else {
+					throw new IllegalArgumentException("Unknown command: " + words[0]);
+				}
+			}
+		}
+	}
+
+	private static Actionable createAction(String action, String args) throws IllegalArgumentException {
+		switch (CommandAction.valueOf(action)) {
+		case PLACE:
+			// separate the coordinates
+			String[] coord = args.split(",");
+			Position position;
+			try{
+				int x = Integer.parseInt(coord[0]);
+				int y = Integer.parseInt(coord[1]);
+				Orientation o = Orientation.valueOf(coord[2]);
+				position = new Position(x, y, o);
+			} catch(IllegalArgumentException e) {
+				throw new IllegalArgumentException("Unknown command: " + coord[2]);
+			}
+			return new PlaceAction(position);
+		case MOVE:
+			return new MoveAction();
+		case LEFT:
+			return new LeftAction();
+		case RIGHT:
+			return new RightAction();
+		case REPORT:
+			return new ReportAction();
+		}
+		return null;
+	}
 }
--- src/test/java/au/com/rea/util/RobotCommandUtilTest.java
+++ src/test/java/au/com/rea/util/RobotCommandUtilTest.java
@@ -1,14 +1,73 @@
 package au.com.rea.util;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 import org.junit.Test;
+
+import au.com.rea.model.Actionable;
+import au.com.rea.util.RobotCommandUtil.CommandAction;
 
 public class RobotCommandUtilTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testNormalInput() {
+		String input = "    MOVE  PLACE 0,2,NORTH RIGHT    MOVE REPORT   ";
+		
+		try {
+			Queue<Actionable> queue = new LinkedList<>();
+			RobotCommandUtil.validCommandAndCreateAction(queue, input);
+			
+			assert queue.poll().getAction() == CommandAction.MOVE;
+			assert queue.poll().getAction() == CommandAction.PLACE;
+			assert queue.poll().getAction() == CommandAction.RIGHT;
+			assert queue.poll().getAction() == CommandAction.MOVE;
+			assert queue.poll().getAction() == CommandAction.REPORT;
+		} catch (IllegalArgumentException e) {
+			fail("Unexpected error!");
+		}
+	}
+	
+	@Test
+	public void testNormalSingleInput() {
+		String input = "REPORT";
+		
+		try {
+			Queue<Actionable> queue = new LinkedList<>();
+			RobotCommandUtil.validCommandAndCreateAction(queue, input);
+			assert queue.poll().getAction() == CommandAction.REPORT;
+		} catch (IllegalArgumentException e) {
+			fail("Unexpected error!");
+		}
+	}
+	
+	@Test
+	public void testBadPlacePosition() {
+		String input = "PLACE 0,2,huids RIGHT    MOVE REPORT   ";
+		
+		try {
+			Queue<Actionable> queue = new LinkedList<>();
+			RobotCommandUtil.validCommandAndCreateAction(queue, input);
+			assert queue.poll().getAction() == CommandAction.REPORT;
+			fail("Expected an IllegalArgumentException to be thrown");
+		} catch (IllegalArgumentException e) {
+			assert e.getMessage().equals("Unknown command: PLACE 0,2,huids");
+		}
 	}
 
+	@Test
+	public void testBadSyntax() {
+		String input = "0,2,NORTH RIGHT    MOVE REPORT   ";
+		
+		try {
+			Queue<Actionable> queue = new LinkedList<>();
+			RobotCommandUtil.validCommandAndCreateAction(queue, input);
+			assert queue.poll().getAction() == CommandAction.REPORT;
+			fail("Expected an IllegalArgumentException to be thrown");
+		} catch (IllegalArgumentException e) {
+			assert e.getMessage().equals("Unknown command: 0,2,NORTH");
+		}
+	}
 }
